89e69d14f6fcc30c863be7e5014a6ca8b2cb35f1
Luis Goyburo <luisgoyburo@balcarieditores.pe>
2021-04-16 19:41:37 -0500

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const { getRouteMatcher } = require("next/dist/next-server/lib/router/utils");
+
+const about = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
